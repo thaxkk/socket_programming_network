0efd12dffaf5e5892f935431c8be85e089802c78
aonchalisa <aonchalisa@gmail.com>
2025-10-14 15:39:56 +0700

    base

---
+++ frontend/vite.config.js
@@ -0,0 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // 🔹 แก้เป็น port ของ backend คุณ
+        changeOrigin: true,
+      },
+    },
+  },
+})
