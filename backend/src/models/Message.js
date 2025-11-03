5d4377b70c0d675c903d273caa7e612b4b118b45
whatiskk <kwankaothanyaporn2015@gmail.com>
2025-11-04 00:47:23 +0700

    Revert "group implement"
    This reverts commit 0ef7f77702c94d398b9d96dbb9e00c4f3109c2ab.

--- backend/src/models/Message.js
+++ backend/src/models/Message.js
@@ -10,50 +10,19 @@ const messageSchema = new mongoose.Schema(
     receiverId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: false, // Not required for group messages
-    },
-    groupId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Group",
-      required: false, // Required for group messages
+      required: true,
     },
     text: {
       type: String,
+      trim: true,
+      maxlength: 2000,
     },
     image: {
       type: String,
     },
-    // Track who has read the message in groups
-    readBy: [
-      {
-        userId: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "User",
-        },
-        readAt: {
-          type: Date,
-          default: Date.now,
-        },
-      },
-    ],
   },
   { timestamps: true }
 );
-
-// Validation: message must have either receiverId or groupId
-messageSchema.pre("save", function (next) {
-  if (!this.receiverId && !this.groupId) {
-    next(new Error("Message must have either receiverId or groupId"));
-  } else if (this.receiverId && this.groupId) {
-    next(new Error("Message cannot have both receiverId and groupId"));
-  } else {
-    next();
-  }
-});
-
-// Indexes for better query performance
-messageSchema.index({ senderId: 1, receiverId: 1 });
-messageSchema.index({ groupId: 1, createdAt: -1 });
 
 const Message = mongoose.model("Message", messageSchema);
 
